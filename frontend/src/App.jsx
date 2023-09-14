7441416d77f235a25b0f94485dc063d240ca1a91
paras-017 <paraschauhan0017@gmail.com>
2023-09-14 20:02:26 +0530

    base setuped for UI

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+
+function App() {
+  const [notes, setNotes] = useState(null)
+  const fetchNotes = ()=>{
+    //fetch notes
+    // display on UI
+  }
+  return (
+    <>
+      <h1>hello There</h1>
+    </>
+  )
+}
+
+export default App
